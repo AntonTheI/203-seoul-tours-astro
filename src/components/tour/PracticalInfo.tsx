7e5e4e1cb1f4f70d0fc7ddb9a41e95fb6d328823
Anton I <anton333mai@gmail.com>
2026-03-12 18:23:13 +0900

    add wordpress api connection working

--- src/components/tour/PracticalInfo.tsx
+++ src/components/tour/PracticalInfo.tsx
@@ -1,14 +1,36 @@
 import { Separator } from "../ui/separator";
 
-const tourInfo = [
-  { label: "Duration", value: "2 hours" },
-  { label: "Walking level", value: "Relaxed pace · Flat streets" },
-  { label: "Meeting point", value: "Jegi-dong metro station" },
-  { label: "Start time", value: "9:00-15:00" },
-  { label: "Best timing", value: "Morning visits tend to be most fun" },
-];
+// const tourInfo = [
+//   { label: "Duration", value: "2 hours" },
+//   { label: "Walking level", value: "Relaxed pace · Flat streets" },
+//   { label: "Meeting point", value: "Jegi-dong metro station" },
+//   { label: "Start time", value: "9:00-15:00" },
+//   { label: "Best timing", value: "Morning visits tend to be most fun" },
+// ];
 
-const PracticalInfo = ({ className }: { className?: string }) => {
+interface TourInfo {
+  duration: string;
+  walking_level: string;
+  meeting_point: string;
+  start_time: string;
+  best_timing: string;
+}
+
+const PracticalInfo = ({
+  className,
+  info,
+}: {
+  className?: string;
+  info: TourInfo;
+}) => {
+  const tourInfo = [
+    { label: "Duration", value: info.duration },
+    { label: "Walking level", value: info.walking_level },
+    { label: "Meeting point", value: info.meeting_point },
+    { label: "Start time", value: info.start_time },
+    { label: "Best timing", value: info.best_timing },
+  ];
+
   return (
     <div className={className}>
       <h4 className="accent-label mb-4">PRACTICAL INFORMATION</h4>
