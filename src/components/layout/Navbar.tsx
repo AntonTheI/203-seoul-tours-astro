cb9318a70a9913b6f15cc7d2446e0ac22d74fd99
Anton I <anton333mai@gmail.com>
2026-03-23 19:30:34 +0900

    fix change max width on a header to 1280px

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = ({ currentPath = "" }: Props) => {
     <nav
       className={`fixed w-full py-2 top-0 z-50 transition-all duration-300 bg-natural-light ${scrolled ? "border-b-[0.5px] border-gray-200 bg-natural-light/80 backdrop-blur-xs" : "text-dark-chromatic-teal"}`}
     >
-      <div className="container mx-auto px-4">
+      <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center lg:hidden">
           <MobileMenu currentPath={currentPath} />
           <Book />
